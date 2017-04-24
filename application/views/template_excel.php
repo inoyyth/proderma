8b679b1ad3d16f4b5d615d4d9091658eaa152f38
inoy <supri170845@gmail.com>
2017-04-24 08:53:56 +0700

    refresh cache file

--- application/views/template_excel.php
+++
@@ -1,8 +0,0 @@
-<?php 
-header('Content-Type: application/force-download');
-header('Content-disposition: attachment; filename='.$file_name.'.xls');
-$this->load->view($template_excel,$list);
-header("Pragma: ");
-header("Cache-Control: ");
-//echo $_REQUEST['datatodisplay'];
-?>
