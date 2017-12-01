8dd13e7687fbfd5fdf9dce8f8bf63936eb87276f
inoyyth <supri170845@gmail.com>
2017-12-01 21:45:19 +0700

    update view dashboard

--- application/modules/dashboard/views/main.php
+++ application/modules/dashboard/views/main.php
@@ -1,8 +1,15 @@
+<?php
+    if(isset($this->sessionGlobal['id_branch'])) {
+        $branch = $this->sessionGlobal['id_branch'];
+    } else {
+        $branch = "all";
+    }
+?>
 <link rel="stylesheet" href="<?php echo base_url(); ?>themes/assets/plugin/Highcharts-5.0.14/code/css/highcharts.css" />
 <div class="row">
     <div class="space-6"></div>
 
-    <div class="col-sm-7 infobox-container">
+    <div class="col-sm-12 infobox-container">
         <div class="infobox infobox-green">
             <div class="infobox-icon">
                 <i class="ace-icon fa fa-cubes"></i>
@@ -38,7 +45,7 @@
 
         <div class="space-6"></div>
         
-        <div class="infobox infobox-grey infobox-small infobox-dark">
+        <!--<div class="infobox infobox-grey infobox-small infobox-dark">
             <div class="infobox-icon">
                 <i class="ace-icon fa fa-shopping-bag"></i>
             </div>
@@ -72,11 +79,18 @@
                 <div class="infobox-content"><?php echo $due_date['total'];?></div>
             </div>
         </div>
-    </div>
+    </div>-->
 
     <div class="vspace-12-sm"></div>
-
-    <div class="col-sm-5">
+    <div class="col-lg-12" id="area-chart">
+        <div class="col-lg-6">
+            <div id="container-so-1" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+        </div>
+        <div class="col-lg-6">
+            <div id="container-income-1" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+        </div>
+    </div>
+    <!--<div class="col-sm-5">
         <div class="widget-box">
             <div class="widget-header widget-header-flat widget-header-small">
                 <h5 class="widget-title">
@@ -92,7 +106,7 @@
                 </div><!-- /.widget-main -->
             </div><!-- /.widget-body -->
         </div><!-- /.widget-box -->
-    </div><!-- /.col -->
+    <!--</div><!-- /.col -->
 </div><!-- /.row -->
 
 <div class="hr hr32 hr-dotted"></div>
@@ -100,36 +114,127 @@
 <script src="<?php echo base_url(); ?>themes/assets/plugin/Highcharts-5.0.14/code/js/highcharts.js"></script>
 <script>
 $(document).ready(function () {
+    var id_branch = "<?php echo $branch;?>";
+    console.log(id_branch);
     // Build the chart
-    Highcharts.chart('container', {
-        chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false,
-            type: 'pie',
-            height: 300,
-        },
-        title: {
-            text: 'Statistic Of Areas'
-        },
-        tooltip: {
-            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-        },
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: false
-                },
-                showInLegend: true
-            }
-        },
-        series: [{
-            name: 'Brands',
-            colorByPoint: true,
-            data: <?php echo $mapping_area; ?>
-        }]
-    });
+    $("#area-chart").empty();
+    if (id_branch !== "all") {
+        reportso(id_branch,1);
+        reportincome(id_branch,1);
+    } else {
+        reportso("all",1);
+        reportincome("all",1);
+    }
+    
 });
+
+function reportso(branch,pos) {
+    var d = new Date();
+    var html = '<div class="col-lg-6">' +
+                    '<div id="container-so-'+pos+'" style="min-width: 310px; height: 400px; margin: 0 auto"></div>' +
+                ' </div>';
+    $("#area-chart").append(html);
+
+    var dt = {branch: branch, month: d.getMonth(), year: d.getYear()};
+
+    $.ajax({
+        type: "POST",
+        url: '<?php echo base_url('r_penjualan/getReport');?>',
+        data: dt,
+        success: function(i, data){
+            console.log(data);
+        },
+        dataType: 'json'
+    })
+    .done(function (data){
+        Highcharts.chart('container-so-'+pos, {
+            chart: {
+                type: 'line'
+            },
+            title: {
+                text: data.title
+            },
+            subtitle: {
+                text: data.subtitle
+            },
+            xAxis: {
+                categories: data.category
+            },
+            yAxis: {
+                title: {
+                    text: 'Value'
+                }
+            },
+            navigation: {
+                buttonOptions: {
+                        enabled: true
+                }
+            },
+            plotOptions: {
+                line: {
+                    dataLabels: {
+                        enabled: true
+                    },
+                    enableMouseTracking: false
+                }
+            },
+            series: [{
+                name: 'Sales Order',
+                data: data.value
+            }]
+        });
+    });
+}
+
+function reportincome(branch,pos) {
+    var d = new Date();
+    var html = '<div class="col-lg-6">' +
+                    '<div id="container-income-'+pos+'" style="min-width: 310px; height: 400px; margin: 0 auto"></div>' +
+                ' </div>';
+    $("#area-chart").append(html);
+
+    var dt = {branch: branch, month: d.getMonth(), year: d.getYear()};
+    $.ajax({
+        type: "POST",
+        url: '<?php echo base_url('r_pendapatan/getReport');?>',
+        data: dt,
+        success: function(i, data){
+            console.log(data);
+        },
+        dataType: 'json'
+    })
+    .done(function (data){
+        Highcharts.chart('container-income-'+pos, {
+            chart: {
+                type: 'line'
+            },
+            title: {
+                text: data.title
+            },
+            subtitle: {
+                text: data.subtitle
+            },
+            xAxis: {
+                categories: data.category
+            },
+            yAxis: {
+                title: {
+                    text: 'Value'
+                }
+            },
+            plotOptions: {
+                line: {
+                    dataLabels: {
+                        enabled: true
+                    },
+                    enableMouseTracking: true
+                }
+            },
+            series: [{
+                name: 'Income (Rp)',
+                data: data.value
+            }]
+        });
+    });
+}
 </script>
